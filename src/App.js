0e321134fe908960a3d91612acd5e3b3cb8d3d07
pablodesousa <pablo.de-sousa@epitech.eu>
2020-04-02 11:28:17 +0200

    add front for object and login page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World !!!
-        </p>
-      </header>
+
     </div>
   );
 }
